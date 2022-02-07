377a54cde8811d1478061929036d30d7646d0065
anuragchoudhary1910 <anuragchoudhary1910@gmail.com>
2022-02-07 19:16:51 +0530

    update

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ io.on('connection', (socket) => {
   socket.on('online', (id) => {
     io.emit('online', id);
   });
+  socket.on('typing', (senderId, recieverId) => {
+    io.emit('typing', senderId, recieverId);
+  });
+  socket.on('notTyping', (senderId, recieverId) => {
+    io.emit('notTyping', senderId, recieverId);
+  });
   socket.on('disconnect', () => {
     io.emit('offline', socket.handshake.query.id);
   });
